600ddeabc5dd88824992ddf62922de138dfe67e7
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2026-04-22 15:49:08 -0300

    player state machine

---
+++ player/states/run.gd
@@ -0,0 +1,27 @@
+class_name PlayerStateRun extends PlayerState
+
+#region // state references
+#endregion
+
+# What happens when this state is initialized?
+func init() -> void:
+	pass
+
+# What happens when we enter this state?
+func enter() -> void: 
+	pass
+
+# What happens when we exit this state? 
+func exit() -> void:
+	pass 
+
+# What happens when an input is pressed?
+func handle_input(_event: InputEvent) -> PlayerState:
+	return next_state
+
+# What happens each process tick in this state?
+func process(_delta: float) -> PlayerState:
+	return next_state
+	
+func physics_process(_delta: float) -> PlayerState:
+	return next_state
